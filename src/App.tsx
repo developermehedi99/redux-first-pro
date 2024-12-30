624082b94d07474268d6dcc7896b755778f50c60
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-12-31 00:28:57 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>React redux projects</h2>
+      <h2>00</h2>
+      <button>increment</button>
+      <button>decrement</button>
+    </div>
+  );
+}
+
+export default App;
